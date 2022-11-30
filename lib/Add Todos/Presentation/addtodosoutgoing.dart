4aeb65dbb1c4e4fb97160e1ab5fc09c5eeb76a7e
shreeecreation <shreeecreation720#gmail.com>
2022-11-30 15:42:23 +0545

    todos finished

--- lib/Add Todos/Presentation/addtodosoutgoing.dart
+++ lib/Add Todos/Presentation/addtodosoutgoing.dart
@@ -15,6 +15,7 @@ class AddTodosOut extends StatelessWidget {
   TextEditingController openingBlc = TextEditingController();
   TextEditingController address = TextEditingController();
   final formGlobalKey = GlobalKey<FormState>();
+  final formGlobalKey2 = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,143 +28,152 @@ class AddTodosOut extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Padding(padding: EdgeInsets.all(8.0), child: Text("Party Information", style: TextStyle(fontSize: 18))),
+              const Padding(
+                  padding: EdgeInsets.all(8.0), child: Text("Party Information", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800))),
               widthLongTextField(context),
               balance(context),
-              ElevatedButton(
-                  onPressed: () async {
-                    if (formGlobalKey.currentState!.validate()) {
-                      formGlobalKey.currentState!.save();
-                      DateTime now = DateTime.now();
-                      String formattedDate = DateFormat('MMMM d , y – kk:mm a').format(now);
-                      await DatabaseHelper.instance.add(PartyModel(
-                          name: partyName.text,
-                          phone: phoneNo.text,
-                          blc: int.parse(openingBlc.text),
-                          address: address.text,
-                          date: formattedDate,
-                          mode: 0,
-                          totalblc: int.parse(openingBlc.text)));
-                      await Future.delayed(const Duration(seconds: 1), () {
-                        SchedulerBinding.instance.addPostFrameCallback((_) {
-                          Navigator.pushAndRemoveUntil<dynamic>(
-                            context,
-                            MaterialPageRoute<dynamic>(
-                              builder: (BuildContext context) => const HomePage(),
-                            ),
-                            (route) => false,
-                          );
-                        });
-                      });
-                    }
-                  },
-                  child: const Text("Save")),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 2,
+                    child: savebutton(context),
+                  ),
+                ],
+              ),
             ],
           ),
         ));
   }
 
-  Container balance(BuildContext context) {
-    return Container(
-      color: Colors.black12,
-      child: Column(
-        children: [
-          const SizedBox(height: 20),
-          Container(
-            color: Colors.white,
-            child: Theme(
-              data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-              child: ExpansionTile(
-                title: const Text(
-                  "Opening Balance",
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
-                ),
-                textColor: Colors.black,
-                iconColor: Colors.black,
-                trailing: const Icon(Icons.add),
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        SizedBox(
-                            height: 50,
-                            width: MediaQuery.of(context).size.width / 2.5,
-                            child: TextFormField(
-                                inputFormatters: <TextInputFormatter>[
-                                  FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]")),
-                                ], // O
-                                cursorColor: Colors.black,
-                                controller: openingBlc,
-                                keyboardType: TextInputType.number,
-                                decoration: const InputDecoration(
-                                    border: InputBorder.none,
-                                    filled: true,
-                                    fillColor: Color.fromARGB(31, 128, 128, 128),
-                                    hintText: 'Enter Amount',
-                                    hintStyle: TextStyle(fontSize: 16)))),
-                        const SizedBox(width: 20),
-                        SizedBox(
-                            height: 50,
-                            width: MediaQuery.of(context).size.width / 2.5,
-                            child: TextFormField(
-                                cursorColor: Colors.black,
-                                decoration: const InputDecoration(
-                                    border: InputBorder.none,
-                                    filled: true,
-                                    fillColor: Color.fromARGB(31, 128, 128, 128),
-                                    hintText: 'Date',
-                                    hintStyle: TextStyle(fontSize: 16))))
-                      ],
+  Widget savebutton(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: CTheme.kPrimaryColor),
+        onPressed: () async {
+          if (formGlobalKey.currentState!.validate() && formGlobalKey2.currentState!.validate()) {
+            formGlobalKey.currentState!.save();
+            formGlobalKey2.currentState!.save();
+            DateTime now = DateTime.now();
+            String formattedDate = DateFormat('MMMM d , y – kk:mm a').format(now);
+            await DatabaseHelper.instance.add(PartyModel(
+                name: partyName.text,
+                phone: phoneNo.text,
+                blc: int.parse(openingBlc.text),
+                address: address.text,
+                date: formattedDate,
+                mode: 0,
+                totalblc: int.parse(openingBlc.text)));
+            await Future.delayed(const Duration(seconds: 1), () {
+              SchedulerBinding.instance.addPostFrameCallback((_) {
+                Navigator.pushAndRemoveUntil<dynamic>(
+                    context,
+                    MaterialPageRoute<dynamic>(
+                      builder: (BuildContext context) => const HomePage(),
                     ),
-                  ),
-                ],
-              ),
-            ),
+                    (route) => false);
+              });
+            });
+          }
+        },
+        child: const Text("Save"));
+  }
+
+  Container balance(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('y – kk:mm a').format(now);
+    return Container(
+        color: Colors.black12,
+        child: Column(children: [
+          const SizedBox(height: 20),
+          Form(
+            key: formGlobalKey2,
+            child: Container(
+                color: Colors.white,
+                child: Theme(
+                    data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+                    child: ExpansionTile(
+                        title: const Text(
+                          "Opening Balance",
+                          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                        ),
+                        textColor: Colors.black,
+                        iconColor: Colors.black,
+                        trailing: const Icon(Icons.add),
+                        children: <Widget>[
+                          Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                                SizedBox(
+                                    height: 60,
+                                    width: MediaQuery.of(context).size.width / 2.5,
+                                    child: TextFormField(
+                                        inputFormatters: <TextInputFormatter>[
+                                          FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]")),
+                                        ], // O
+                                        validator: (text) {
+                                          if (text == null || text.isEmpty) {
+                                            return 'Error !!!';
+                                          }
+
+                                          return null;
+                                        },
+                                        cursorColor: Colors.black,
+                                        controller: openingBlc,
+                                        keyboardType: TextInputType.number,
+                                        decoration: const InputDecoration(
+                                            border: InputBorder.none,
+                                            filled: true,
+                                            fillColor: Color.fromARGB(31, 128, 128, 128),
+                                            hintText: 'Enter Amount',
+                                            hintStyle: TextStyle(fontSize: 16)))),
+                                const SizedBox(width: 20),
+                                SizedBox(
+                                    height: 50,
+                                    width: MediaQuery.of(context).size.width / 2.5,
+                                    child: TextFormField(
+                                        initialValue: formattedDate,
+                                        cursorColor: Colors.black,
+                                        decoration: const InputDecoration(
+                                            border: InputBorder.none,
+                                            filled: true,
+                                            fillColor: Color.fromARGB(31, 128, 128, 128),
+                                            hintText: 'Date',
+                                            hintStyle: TextStyle(fontSize: 16))))
+                              ]))
+                        ]))),
           ),
           const SizedBox(height: 20),
           Container(
-            color: Colors.white,
-            child: Theme(
-              data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-              child: ExpansionTile(
-                title: const Text(
-                  "Additional Details",
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
-                ),
-                textColor: Colors.black,
-                iconColor: Colors.black,
-                trailing: const Icon(Icons.add),
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: SizedBox(
-                            height: 50,
-                            width: MediaQuery.of(context).size.width - 20,
-                            child: TextFormField(
-                                controller: address,
-                                cursorColor: Colors.black,
-                                decoration: const InputDecoration(
-                                    border: InputBorder.none,
-                                    filled: true,
-                                    fillColor: Color.fromARGB(31, 128, 128, 128),
-                                    hintText: 'Add Address',
-                                    hintStyle: TextStyle(fontSize: 16)))),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-        ],
-      ),
-    );
+              color: Colors.white,
+              child: Theme(
+                  data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+                  child: ExpansionTile(
+                      title: const Text("Additional Details", style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500)),
+                      textColor: Colors.black,
+                      iconColor: Colors.black,
+                      trailing: const Icon(Icons.add),
+                      children: <Widget>[
+                        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                                height: 50,
+                                width: MediaQuery.of(context).size.width - 20,
+                                child: TextFormField(
+                                    controller: address,
+                                    cursorColor: Colors.black,
+                                    decoration: const InputDecoration(
+                                        border: InputBorder.none,
+                                        filled: true,
+                                        fillColor: Color.fromARGB(31, 128, 128, 128),
+                                        hintText: 'Add Address',
+                                        hintStyle: TextStyle(fontSize: 16)))),
+                          )
+                        ])
+                      ]))),
+          const SizedBox(height: 20)
+        ]));
   }
 
   Container widthLongTextField(BuildContext context) {
@@ -182,6 +192,7 @@ class AddTodosOut extends StatelessWidget {
                   child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: TextFormField(
+                          textCapitalization: TextCapitalization.words,
                           controller: partyName,
                           cursorColor: Colors.black,
                           validator: (text) {
@@ -208,12 +219,17 @@ class AddTodosOut extends StatelessWidget {
                             if (text == null || text.isEmpty) {
                               return 'Phone Number can\'t be empty';
                             }
+                            if (text.length <= 9) {
+                              return 'Enter a valid phone number';
+                            }
 
                             return null;
                           },
+                          maxLength: 10,
                           cursorColor: Colors.black,
                           keyboardType: TextInputType.number,
                           decoration: const InputDecoration(
+                              counterText: "",
                               border: InputBorder.none,
                               filled: true,
                               fillColor: Color.fromARGB(31, 128, 128, 128),
